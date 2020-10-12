e10c047eb1b3fda53f8b62dbf0cf75456ca4102c
Brandon West <i.am.ableton1@gmail.com>
2020-10-11 18:58:53 -0700

    Removed debugging print statement

--- mp4_to_proRes.py
+++ mp4_to_proRes.py
@@ -10,7 +10,6 @@ for filename in os.listdir(path):
     output_path = os.path.join(path,output_name)
 
     if filename.endswith(".mp4"):
-        #print("ffmpeg -i {0} -c:v prores_ks -profile:v -1 -c:a pcm_s32le {1}.mov".format(input_path, output_name))
         os.system("ffmpeg -i {0} -c:v prores_ks -profile:v -1 -c:a pcm_s32le {1}.mov".format(input_path, output_path))
         print(filename)
     else:
